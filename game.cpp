42ad86345a9cebd13b218c5f0812eae80a757731
diephieunghia123 <38271721+diephieunghia123@users.noreply.github.com>
2018-04-11 22:53:59 +0700

    vatcandichuyen

---
+++ game.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include "lib.h"
+
+
+void main()
+{
+	char *vatcan;
+	tao(vatcan);
+	dichuyen(5, 5, vatcan);
+	_getch();
+}
---
+++ lib.h
@@ -0,0 +1,4 @@
+void gotoxy(int x, int y);
+void tao(char *&a);
+void in(char *a,int x,int y);
+void dichuyen(int x, int y, char *a);
