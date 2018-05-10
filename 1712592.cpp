77db72a735f7b383d6a5e336048bd9d8928bc014
Hac <laohacsieudeptrai@gmail.com>
2018-05-10 23:37:00 +0700

    fix lỗi không input được

--- 1712592.cpp
+++ 1712592.cpp
@@ -15,15 +15,15 @@ O buffer[30][30]; // khai báo để sử dụng toàn bài
 void Input(int score)
 {
 	system("cls");
-	FILE *f = fopen("Player.txt", "at");
 	PLAYER player;
 	char c;
-	rewind(stdin);
+	_getch();
 	gotoxy(0, 14);
 	printf("Diem: %d\n", score);
 	gotoxy(0, 15);
 	printf("Nhap ten: ");
 	scanf("%[^\n]s &c", player.Name, &c);
+	FILE *f = fopen("Player.txt", "at");
 	fprintf(f, "%s\n", player.Name);
 	fprintf(f, "	%d\n", score);
 	fclose(f);
@@ -469,6 +469,7 @@ void playGame() // Dành cho 1 người chơi (Solo)
 		//Game over
 		if (GameOver(thing))
 		{
+			flushall();
 			Input(score);
 			return;
 		}
